7128034b3b4204adbb37814a6f45be1d89d466ba
Daniel <danielkibret8@gmail.com>
2026-02-06 00:25:19 +0300

    auth

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 import express from 'express';
+import dotenv from 'dotenv';
 
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
 const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Server testing');
+});
+
+app.listen(PORT, () => {
+  console.log(`I am running on port localhost:${PORT}`);
+});
