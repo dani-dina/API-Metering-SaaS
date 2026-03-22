86744623cb9eb77a8c68f7db0f014443a128eeb2
Daniel <danielkibret8@gmail.com>
2026-03-22 23:50:07 +0300

    db conn

--- src/configs/db.js
+++ src/configs/db.js
@@ -1,7 +1,14 @@
 import mongoose from 'mongoose';
 
 const conn = () => {
-
+  mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/api_metering', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }).then(() => {
+    console.log('MongoDB connected');
+  }).catch((err) => {
+    console.error('MongoDB connection error:', err);
+  });
 }
 
 export default conn;
